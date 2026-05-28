7bec0576e8356344891257c6fde53a937fe1b7ea
richfjiq <rfjiq1986@gmail.com>
2026-05-28 17:53:45 -0600

    abstract classes

---
+++ app/src/main/java/com/richdev/objectorientedprogramming/abstract/AbstractTest.kt
@@ -0,0 +1,12 @@
+package com.richdev.objectorientedprogramming.abstract
+
+fun main(args: Array<String>) {
+    // var vehicle = Vehicle()
+    var car = Car(2021)
+    car.speed = 300
+    println("Name  : ${car.vehicleName("Ferrari")} \n " +
+            "Type  : ${car.vehicleType("Car")} \n " +
+            "Model : ${car.model} \n " +
+            "Speed : ${car.speed}"
+    )
+}
---
+++ app/src/main/java/com/richdev/objectorientedprogramming/abstract/Car.kt
@@ -0,0 +1,8 @@
+package com.richdev.objectorientedprogramming.abstract
+
+class Car(override var model: Int) : Vehicle() {
+    override fun vehicleName(name: String): String {
+        return name
+    }
+
+}
